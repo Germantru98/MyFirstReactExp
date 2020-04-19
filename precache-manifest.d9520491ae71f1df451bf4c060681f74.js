0da854d5b2bc3b8eecb1df17e335e33d74ffcdd8
Герман Труханов <germantruhanov@gmail.com>
2020-04-19 20:17:22 +0400

    Updates

---
+++ precache-manifest.d9520491ae71f1df451bf4c060681f74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51365ca0c87a00b52949066266a9b37a",
+    "url": "/MyFirstReactExp/index.html"
+  },
+  {
+    "revision": "d993087cbcf69157370c",
+    "url": "/MyFirstReactExp/static/css/main.e98d77f7.chunk.css"
+  },
+  {
+    "revision": "91ed37d1988482195a7c",
+    "url": "/MyFirstReactExp/static/js/2.c4b6d3c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MyFirstReactExp/static/js/2.c4b6d3c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d993087cbcf69157370c",
+    "url": "/MyFirstReactExp/static/js/main.0e8414b9.chunk.js"
+  },
+  {
+    "revision": "9d14446159f44a7b1296",
+    "url": "/MyFirstReactExp/static/js/runtime-main.d8b264f5.js"
+  }
+]);
